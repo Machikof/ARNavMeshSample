a5833d1e7a38280c04b4a95e165cdedde5a748b2
Machinaga Kosuke <machikou@icloud.com>
2021-04-22 14:33:43 +0900

    feat: Runtime NavMeshを実装

---
+++ Assets/Scripts/BuildNavMesh.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshSurface))]
+public class BuildNavMesh : MonoBehaviour
+{
+    [Header("ベイク更新時間"), SerializeField] float bakeUpdateTime;
+    private NavMeshSurface _surface;
+    void Start()
+    {
+        _surface = GetComponent<NavMeshSurface>();
+        StartCoroutine(BakeUpdate());
+    }
+
+    IEnumerator BakeUpdate()
+    {
+        while(true)
+        {
+            _surface.BuildNavMesh();
+
+            yield return new WaitForSeconds(bakeUpdateTime);
+        }
+    }
+}
