e8e8dd4763de2bda8081f6dc750ef7c0e59f63b9
Machinaga Kosuke <machikou@icloud.com>
2021-04-22 14:34:47 +0900

    feat: エージェントを生成

---
+++ Assets/Scripts/CreateAgent.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class CreateAgent : MonoBehaviour
+{
+    [Header("出現させるオブジェクト"), SerializeField] GameObject objectPrefab;
+    ARRaycastManager raycastManager;
+    List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+    bool isSet = false;
+
+    void Start()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        // エディタ上
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            // レイと平面が交差時
+            if (raycastManager.Raycast(Input.GetTouch(0).position, hitResults, TrackableType.All))
+            {
+                SetObject(hitResults[0].pose.position);
+            }
+
+        }
+        // 端末上での動作
+#else
+        if(Input.touchCount > 0)
+        {
+            var touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                // レイと平面が交差時
+                if (raycastManager.Raycast(Input.GetTouch(0).position, hitResults, TrackableType.All))
+                {
+                    SetObject(hitResults[0].pose.position);
+                }
+            }
+        }
+
+#endif
+    }
+
+    void SetObject(Vector3 position)
+    {
+        if (!isSet)
+        {
+            Instantiate(objectPrefab, position, Quaternion.identity);
+            isSet = true;
+        }
+        else
+        {
+            Debug.Log("Object is here now");
+        }
+    }
+}
---
+++ Assets/Scripts/MoveAgent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveAgent : MonoBehaviour
+{
+    [Header("ARカメラ"), SerializeField] GameObject camera;
+    NavMeshAgent agent;
+    Vector3 targetPos;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        targetPos = new Vector3(camera.transform.position.x, transform.position.y, camera.transform.position.z);
+    }
+
+    void Update()
+    {
+        ApproachToCamera();
+    }
+
+    void ApproachToCamera()
+    {
+        if (agent.pathStatus != NavMeshPathStatus.PathInvalid)
+        {
+            Debug.Log("NavMesh is ready");
+            agent.destination = targetPos;
+        }
+        else Debug.Log("NavMesh is not ready");
+    }
+}
